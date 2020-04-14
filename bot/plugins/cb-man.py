df72e83d7a9278b806918a56df39b673019d6c88
odysseusmax <royschristy@gmail.com>
2020-04-14 06:28:14 +0000

    tests

--- bot/plugins/cb-man.py
+++ bot/plugins/cb-man.py
@@ -3,7 +3,10 @@ from pyrogram import Client, Filters, ForceReply
 
 @Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith('man')))
 async def _(c, m):
-    await m.edit_message_text(
-        "Please provide list of seconds you want to extract separated by space.",
+    
+    await m.message.delete()
+    await c.send_message(
+        chat_id=m.from_user.id,
+        text="Please provide list of seconds you want to extract separated by space.",
         reply_markup=ForceReply()
     )
