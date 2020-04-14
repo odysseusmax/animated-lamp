4905aa80cc83b95af7e90cd2b6e347695e068063
odysseusmax <royschristy@gmail.com>
2020-04-14 17:02:56 +0000

    tests

--- bot/plugins/text_reply.py
+++ bot/plugins/text_reply.py
@@ -4,8 +4,7 @@ from pyrogram import Client, Filters, ForceReply
 @Client.on_message(Filters.private & Filters.reply & Filters.text)
 async def _(c, m):
     target = m.reply_to_message
-    media_message = await c.get_messages(m.chat.id, m.reply_to_message.message_id)
-    await target.delete()
+    
     me = await c.get_me()
     if target.from_user.id != me.id:
         return
@@ -15,5 +14,6 @@ async def _(c, m):
     
     if not isinstance(target.reply_markup, ForceReply):
         return
-    
-    print(media_message)
+    await target.delete()
+    media_message = await c.get_messages(m.chat.id, m.reply_to_message.message_id)
+    print(media_message.reply_to_message)
