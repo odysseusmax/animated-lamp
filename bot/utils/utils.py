54b3bcb1f6ea78d0bd8b1c3d1a5ca7c5063a3098
odysseusmax <royschristy@gmail.com>
2020-04-14 03:03:04 +0000

    tests

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -37,7 +37,7 @@ async def generate_screenshots(input_file_link, num=5):
         if not output_folder.exists():
             os.makedirs(output_folder)
         
-        ffmpeg_dur_cmd = f"ffmpeg -i {input_file_link}"
+        ffmpeg_dur_cmd = f"ffmpeg -i '{input_file_link}'"
         output = await run_subprocess(ffmpeg_dur_cmd)
         print(output[1].decode())
         duration = re.findall("Duration: (.*?)\.", output[1].decode())
