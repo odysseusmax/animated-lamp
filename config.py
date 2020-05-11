b6b7340220c32956e3628139851472809a202474
odysseusmax <royschristy@gmail.com>
2020-05-11 07:07:18 +0000

    testing userless setup

--- config.py
+++ config.py
@@ -7,9 +7,6 @@ class Config:
     API_HASH = os.environ.get('API_HASH')
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
     SESSION_NAME = os.environ.get('SESSION_NAME')
-    USER_SESSION_STRING = os.environ.get('USER_SESSION_STRING')
-    MIDDLE_MAN = int(os.environ.get('MIDDLE_MAN'))
-    LINK_GEN_BOT = os.environ.get('LINK_GEN_BOT')
     LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
     DATABASE_URL = os.environ.get('DATABASE_URL')
     AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
