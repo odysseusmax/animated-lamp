4f23010586cdd6bf431b476c413c357eba36175e
odysseusmax <royschristy@gmail.com>
2020-06-13 09:55:41 +0000

    tests manual screenshots

--- bot/plugins/manual_screenshot_1.py
+++ bot/plugins/manual_screenshot_1.py
@@ -8,12 +8,11 @@ from ..config import Config
 
 @ScreenShotBot.on_callback_query(Filters.create(lambda _, query: query.data.startswith('mscht')))
 async def _(c, m):
-    t = m.message.text.markdown.split('\n')
-    print(t)
+    dur = m.message.text.markdown.split('\n')[-1]
     await m.message.delete(True)
     await c.send_message(
         m.from_user.id,
-        f'#manual_screenshot\nNow send your list of seconds separated by `,`(comma).\nEg: `0,10,40,60,120`.\nThis will generate screenshots at 0, 10, 40, 60, and 120 seconds. \n\n* The list can have a maximum of 10 valid positions.\n* The position has to be greater than or equal to 0, or less than the video length in order to be valid.',
+        f'#manual_screenshot\n{dur}\nNow send your list of seconds separated by `,`(comma).\nEg: `0,10,40,60,120`.\nThis will generate screenshots at 0, 10, 40, 60, and 120 seconds. \n\n* The list can have a maximum of 10 valid positions.\n* The position has to be greater than or equal to 0, or less than the video length in order to be valid.',
         reply_to_message_id=m.message.reply_to_message.message_id,
         reply_markup=ForceReply()
     )
