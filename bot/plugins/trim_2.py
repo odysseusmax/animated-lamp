fcce6c1d98903a33e951caab5eb6bd4ae8bc94c4
odysseusmax <royschristy@gmail.com>
2020-06-13 09:28:42 +0000

    testing manual screenshot generation

--- bot/plugins/trim_2.py
+++ bot/plugins/trim_2.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import Filters, ForceReply
 
 from ..config import Config
-from ..utils import trim_fn
+from ..utils import trim_fn, manual_screenshot_fn
 from ..screenshotbot import ScreenShotBot
 
 
@@ -25,4 +25,7 @@ async def _(c, m):
         print('not ForceReply')
         return
     
-    asyncio.create_task(trim_fn(c, m))
+    if m.reply_to_message.text.startswith('#trim_video'):
+        asyncio.create_task(trim_fn(c, m))
+    else:
+        asyncio.create_task(manual_screenshot_fn(c, m))
--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,1 +1,5 @@
 from .utils import *
+from .manual_screenshot import manual_screenshot_fn
+from .sample import sample_fn
+from .trim import trim_fn
+from .screenshot import screenshot_fn
