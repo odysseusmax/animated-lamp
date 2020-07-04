77fd1dc91d531f3a3ffc42924c782af8200817cf
odysseusmax <royschristy@gmail.com>
2020-07-04 04:34:35 +0000

    fix typo

--- bot/plugins/admin/broadcast.py
+++ bot/plugins/admin/broadcast.py
@@ -44,7 +44,7 @@ async def broadcast_(c, m):
             break
     
     out = await m.reply_text(
-        text = f"Broadcast initiated! You will be notified with log file when all the users are notified."
+        text = f"Broadcast initiated! You will be notified with log file when all the users are notified.",
         reply_markup = InlineKeyboardMarkup(
             [
                 [
