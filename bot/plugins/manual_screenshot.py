977c74232551ba8ee50ff5641ddb10710258cf40
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-07-10 12:25:13 +0530

    Create manual_screenshot.py

---
+++ bot/plugins/manual_screenshot.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from pyrogram import Filters, ForceReply
+
+from ..screenshotbot import ScreenShotBot
+from ..config import Config
+
+
+@ScreenShotBot.on_callback_query(Filters.create(lambda _, query: query.data.startswith('mscht')))
+async def _(c, m):
+    dur = m.message.text.markdown.split('\n')[-1]
+    await m.message.delete(True)
+    await c.send_message(
+        m.from_user.id,
+        f'#manual_screenshot\n\n{dur}\n\nNow send your list of seconds separated by `,`(comma).\nEg: `0,10,40,60,120`.\nThis will generate screenshots at 0, 10, 40, 60, and 120 seconds. \n\n1. The list can have a maximum of 10 valid positions.\n2. The position has to be greater than or equal to 0, or less than the video length in order to be valid.',
+        reply_to_message_id=m.message.reply_to_message.message_id,
+        reply_markup=ForceReply()
+    )
