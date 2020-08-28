f910682861e7b2eaad1a1dd83c5fd388e5e6fc74
odysseusmax <royschristy@gmail.com>
2020-08-28 12:23:29 +0530

    test timeout

--- bot/utils/manual_screenshot.py
+++ bot/utils/manual_screenshot.py
@@ -64,111 +64,114 @@ async def manual_screenshot_fn(c, m):
     
     try:
         async with timeout(Config.TIMEOUT) as cm:
-            start_time = time.time()
+            try:
+                start_time = time.time()
             
-            if typ == 2:
-                file_link = media_msg.text
-            else:
-                file_link = generate_stream_link(media_msg)
-            
-            duration = await get_duration(file_link)
-            if isinstance(duration, str):
-                await snt.edit_text("😟 Sorry! I cannot open the file.")
-                
-                l = await media_msg.forward(Config.LOG_CHANNEL)
-                await l.reply_text(f'stream link : {file_link}\n\nRequested manual screenshots\n\n{duration}', True)
-                c.CURRENT_PROCESSES[chat_id] -= 1
-                return
-            
-            valid_positions = []
-            invalid_positions = []
-            for pos in raw_user_input:
-                if pos<0 or pos>duration:
-                    invalid_positions.append(str(pos))
+                if typ == 2:
+                    file_link = media_msg.text
                 else:
-                    valid_positions.append(pos)
-            
-            if not valid_positions:
-                await snt.edit_text("😟 Sorry! None of the given positions where valid!")
-                c.CURRENT_PROCESSES[chat_id] -= 1
-                return
-            
-            if len(valid_positions) > 10:
-                await snt.edit_text(f"😟 Sorry! Only 10 screenshots can be generated. Found {len(valid_positions)} valid positions in your request")
-                c.CURRENT_PROCESSES[chat_id] -= 1
-                return
-            
-            if invalid_positions:
-                invalid_position_str = ', '.join(invalid_positions)
-                txt = f"Found {len(invalid_positions)} invalid positions ({invalid_position_str}).\n\n😀 Generating screenshots after ignoring these!."
-            else:
-                txt = '😀 Generating screenshots!.'
-            
-            await snt.edit_text(txt)
-            
-            screenshots = []
-            watermark = await c.db.get_watermark_text(chat_id)
-            watermark_color_code = await c.db.get_watermark_color(chat_id)
-            watermark_color = Config.COLORS[watermark_color_code]
-            as_file = await c.db.is_as_file(chat_id)
-            font_size = await c.db.get_font_size(chat_id)
-            ffmpeg_errors = ''
-            
-            width, height = await get_dimentions(file_link)
-            fontsize = int((math.sqrt( width**2 + height**2 ) / 1388.0) * Config.FONT_SIZES[font_size])
-            
-            log.info(f"Generating screenshots at positions {valid_positions} from location: {file_link} for {chat_id}")
-            
-            for i, sec in enumerate(valid_positions):
-                thumbnail_template = output_folder.joinpath(f'{i+1}.png')
-                #print(sec)
-                ffmpeg_cmd = f"ffmpeg -hide_banner -ss {sec} -i {shlex.quote(file_link)} -vf \"drawtext=fontcolor={watermark_color}:fontsize={fontsize}:x=20:y=H-th-10:text='{shlex.quote(watermark)}', scale=1280:-1\" -y  -vframes 1 '{thumbnail_template}'"
-                output = await run_subprocess(ffmpeg_cmd)
-                log.debug(output)
-                await snt.edit_text(f'😀 `{i+1}` of `{len(valid_positions)}` generated!')
-                if thumbnail_template.exists():
-                    if as_file:
-                        screenshots.append({
-                            'document':str(thumbnail_template),
-                            'caption':f"ScreenShot at {datetime.timedelta(seconds=sec)}"
-                        })
+                    file_link = generate_stream_link(media_msg)
+                
+                duration = await get_duration(file_link)
+                if isinstance(duration, str):
+                    await snt.edit_text("😟 Sorry! I cannot open the file.")
+                    
+                    l = await media_msg.forward(Config.LOG_CHANNEL)
+                    await l.reply_text(f'stream link : {file_link}\n\nRequested manual screenshots\n\n{duration}', True)
+                    c.CURRENT_PROCESSES[chat_id] -= 1
+                    return
+                
+                valid_positions = []
+                invalid_positions = []
+                for pos in raw_user_input:
+                    if pos<0 or pos>duration:
+                        invalid_positions.append(str(pos))
                     else:
-                        screenshots.append(
-                            InputMediaPhoto(
-                                str(thumbnail_template),
-                                caption=f"ScreenShot at {datetime.timedelta(seconds=sec)}"
-                            )
-                        )
-                    continue
-                ffmpeg_errors += output[0].decode() + '\n' + output[1].decode() + '\n\n'
-            
-            if not screenshots:
-                await snt.edit_text('😟 Sorry! Screenshot generation failed possibly due to some infrastructure failure 😥.')
+                        valid_positions.append(pos)
                 
-                l = await media_msg.forward(Config.LOG_CHANNEL)
-                if ffmpeg_errors:
-                    error_file = f"{uid}-errors.txt"
-                    with open(error_file, 'w') as f:
-                        f.write(ffmpeg_errors)
-                    await l.reply_document(error_file, caption=f"stream link : {file_link}\n\nmanual screenshots {raw_user_input}.")
-                    os.remove(error_file)
+                if not valid_positions:
+                    await snt.edit_text("😟 Sorry! None of the given positions where valid!")
+                    c.CURRENT_PROCESSES[chat_id] -= 1
+                    return
+                
+                if len(valid_positions) > 10:
+                    await snt.edit_text(f"😟 Sorry! Only 10 screenshots can be generated. Found {len(valid_positions)} valid positions in your request")
+                    c.CURRENT_PROCESSES[chat_id] -= 1
+                    return
+                
+                if invalid_positions:
+                    invalid_position_str = ', '.join(invalid_positions)
+                    txt = f"Found {len(invalid_positions)} invalid positions ({invalid_position_str}).\n\n😀 Generating screenshots after ignoring these!."
                 else:
-                    await l.reply_text(f'stream link : {file_link}\n\nmanual screenshots {raw_user_input}.', True)
+                    txt = '😀 Generating screenshots!.'
+                
+                await snt.edit_text(txt)
+                
+                screenshots = []
+                watermark = await c.db.get_watermark_text(chat_id)
+                watermark_color_code = await c.db.get_watermark_color(chat_id)
+                watermark_color = Config.COLORS[watermark_color_code]
+                as_file = await c.db.is_as_file(chat_id)
+                font_size = await c.db.get_font_size(chat_id)
+                ffmpeg_errors = ''
+                
+                width, height = await get_dimentions(file_link)
+                fontsize = int((math.sqrt( width**2 + height**2 ) / 1388.0) * Config.FONT_SIZES[font_size])
+                
+                log.info(f"Generating screenshots at positions {valid_positions} from location: {file_link} for {chat_id}")
+                
+                for i, sec in enumerate(valid_positions):
+                    thumbnail_template = output_folder.joinpath(f'{i+1}.png')
+                    #print(sec)
+                    ffmpeg_cmd = f"ffmpeg -hide_banner -ss {sec} -i {shlex.quote(file_link)} -vf \"drawtext=fontcolor={watermark_color}:fontsize={fontsize}:x=20:y=H-th-10:text='{shlex.quote(watermark)}', scale=1280:-1\" -y  -vframes 1 '{thumbnail_template}'"
+                    output = await run_subprocess(ffmpeg_cmd)
+                    log.debug(output)
+                    await snt.edit_text(f'😀 `{i+1}` of `{len(valid_positions)}` generated!')
+                    if thumbnail_template.exists():
+                        if as_file:
+                            screenshots.append({
+                                'document':str(thumbnail_template),
+                                'caption':f"ScreenShot at {datetime.timedelta(seconds=sec)}"
+                            })
+                        else:
+                            screenshots.append(
+                                InputMediaPhoto(
+                                    str(thumbnail_template),
+                                    caption=f"ScreenShot at {datetime.timedelta(seconds=sec)}"
+                                )
+                            )
+                        continue
+                    ffmpeg_errors += output[0].decode() + '\n' + output[1].decode() + '\n\n'
+                
+                if not screenshots:
+                    await snt.edit_text('😟 Sorry! Screenshot generation failed possibly due to some infrastructure failure 😥.')
+                    
+                    l = await media_msg.forward(Config.LOG_CHANNEL)
+                    if ffmpeg_errors:
+                        error_file = f"{uid}-errors.txt"
+                        with open(error_file, 'w') as f:
+                            f.write(ffmpeg_errors)
+                        await l.reply_document(error_file, caption=f"stream link : {file_link}\n\nmanual screenshots {raw_user_input}.")
+                        os.remove(error_file)
+                    else:
+                        await l.reply_text(f'stream link : {file_link}\n\nmanual screenshots {raw_user_input}.', True)
+                    c.CURRENT_PROCESSES[chat_id] -= 1
+                    return
+                
+                await snt.edit_text(text=f'🤓 You requested {len(valid_positions)} screenshots and {len(screenshots)} screenshots generated, Now starting to upload!')
+                
+                await media_msg.reply_chat_action("upload_photo")
+                
+                if as_file:
+                    aws = [media_msg.reply_document(quote=True, **photo) for photo in screenshots]
+                    await asyncio.gather(*aws)
+                else:
+                    await media_msg.reply_media_group(screenshots, True)
+                
+                await snt.edit_text(f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nIf You find me helpful, please rate me [here](tg://resolve?domain=botsarchive&post=1206).')
                 c.CURRENT_PROCESSES[chat_id] -= 1
-                return
-            
-            await snt.edit_text(text=f'🤓 You requested {len(valid_positions)} screenshots and {len(screenshots)} screenshots generated, Now starting to upload!')
-            
-            await media_msg.reply_chat_action("upload_photo")
-            
-            if as_file:
-                aws = [media_msg.reply_document(quote=True, **photo) for photo in screenshots]
-                await asyncio.gather(*aws)
-            else:
-                await media_msg.reply_media_group(screenshots, True)
-            
-            await snt.edit_text(f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nIf You find me helpful, please rate me [here](tg://resolve?domain=botsarchive&post=1206).')
-            c.CURRENT_PROCESSES[chat_id] -= 1
+            except asyncio.CancelledError:
+                pass
     except asyncio.TimeoutError:
         await snt.edit_text('😟 Sorry! Video trimming failed due to timeout. Your process was taking too long to complete, hence cancelled')
         c.CURRENT_PROCESSES[chat_id] -= 1
--- bot/utils/sample.py
+++ bot/utils/sample.py
@@ -45,8 +45,6 @@ async def sample_fn(c, m):
     else:
         typ = 2
     
-    start_time = time.time()
-    
     await edit_message_text(m, text='Processing your request, Please wait! 😴')
     
     if typ == 2:
@@ -58,53 +56,57 @@ async def sample_fn(c, m):
     
     try:
         async with timeout(Config.TIMEOUT):
-            duration = await get_duration(file_link)
-            if isinstance(duration, str):
-                await edit_message_text(m, text="😟 Sorry! I cannot open the file.")
-                l = await media_msg.forward(Config.LOG_CHANNEL)
-                await l.reply_text(f'stream link : {file_link}\n\nSample video requested\n\n{duration}', True)
+            try:
+                start_time = time.time()
+                duration = await get_duration(file_link)
+                if isinstance(duration, str):
+                    await edit_message_text(m, text="😟 Sorry! I cannot open the file.")
+                    l = await media_msg.forward(Config.LOG_CHANNEL)
+                    await l.reply_text(f'stream link : {file_link}\n\nSample video requested\n\n{duration}', True)
+                    c.CURRENT_PROCESSES[chat_id] -= 1
+                    return
+                
+                reduced_sec = duration - int(duration*10 / 100)
+                sample_duration = await c.db.get_sample_duration(chat_id)
+                
+                start_at = get_random_start_at(reduced_sec, sample_duration)
+                
+                sample_file = output_folder.joinpath(f'sample_video.mkv')
+                subtitle_option = await fix_subtitle_codec(file_link)
+                
+                log.info(f"Generating sample video (duration {sample_duration}s from {start_at}) from location: {file_link} for {chat_id}")
+                
+                ffmpeg_cmd = f"ffmpeg -hide_banner -ss {start_at} -i {shlex.quote(file_link)} -t {sample_duration} -map 0 -c copy {subtitle_option} {sample_file}"
+                output = await run_subprocess(ffmpeg_cmd)
+                log.debug(output)
+                
+                if (not sample_file.exists()) or (os.path.getsize(sample_file) == 0):
+                    await edit_message_text(m, text='😟 Sorry! Sample video generation failed possibly due to some infrastructure failure 😥.')
+                    ffmpeg_output = output[0].decode() + '\n' + output[1].decode()
+                    l = await media_msg.forward(Config.LOG_CHANNEL)
+                    await l.reply_text(f'stream link : {file_link}\n\n duration {sample_duration} sample video generation failed\n\n{ffmpeg_output}', True)
+                    c.CURRENT_PROCESSES[chat_id] -= 1
+                    return
+                
+                thumb = await generate_thumbnail_file(sample_file, uid)
+                
+                await edit_message_text(m, text=f'🤓 Sample video was generated successfully!, Now starting to upload!')
+                
+                await media_msg.reply_chat_action("upload_video")
+                
+                await media_msg.reply_video(
+                        video=str(sample_file), 
+                        quote=True,
+                        caption=f"Sample video. {sample_duration}s from {datetime.timedelta(seconds=start_at)}",
+                        duration=sample_duration,
+                        thumb=thumb,
+                        supports_streaming=True
+                    )
+                
+                await edit_message_text(m, text=f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nIf You find me helpful, please rate me [here](tg://resolve?domain=botsarchive&post=1206).')
                 c.CURRENT_PROCESSES[chat_id] -= 1
-                return
-            
-            reduced_sec = duration - int(duration*10 / 100)
-            sample_duration = await c.db.get_sample_duration(chat_id)
-            
-            start_at = get_random_start_at(reduced_sec, sample_duration)
-            
-            sample_file = output_folder.joinpath(f'sample_video.mkv')
-            subtitle_option = await fix_subtitle_codec(file_link)
-            
-            log.info(f"Generating sample video (duration {sample_duration}s from {start_at}) from location: {file_link} for {chat_id}")
-            
-            ffmpeg_cmd = f"ffmpeg -hide_banner -ss {start_at} -i {shlex.quote(file_link)} -t {sample_duration} -map 0 -c copy {subtitle_option} {sample_file}"
-            output = await run_subprocess(ffmpeg_cmd)
-            log.debug(output)
-            
-            if (not sample_file.exists()) or (os.path.getsize(sample_file) == 0):
-                await edit_message_text(m, text='😟 Sorry! Sample video generation failed possibly due to some infrastructure failure 😥.')
-                ffmpeg_output = output[0].decode() + '\n' + output[1].decode()
-                l = await media_msg.forward(Config.LOG_CHANNEL)
-                await l.reply_text(f'stream link : {file_link}\n\n duration {sample_duration} sample video generation failed\n\n{ffmpeg_output}', True)
-                c.CURRENT_PROCESSES[chat_id] -= 1
-                return
-            
-            thumb = await generate_thumbnail_file(sample_file, uid)
-            
-            await edit_message_text(m, text=f'🤓 Sample video was generated successfully!, Now starting to upload!')
-            
-            await media_msg.reply_chat_action("upload_video")
-            
-            await media_msg.reply_video(
-                    video=str(sample_file), 
-                    quote=True,
-                    caption=f"Sample video. {sample_duration}s from {datetime.timedelta(seconds=start_at)}",
-                    duration=sample_duration,
-                    thumb=thumb,
-                    supports_streaming=True
-                )
-            
-            await edit_message_text(m, text=f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\nIf You find me helpful, please rate me [here](tg://resolve?domain=botsarchive&post=1206).')
-            c.CURRENT_PROCESSES[chat_id] -= 1
+            except asyncio.CancelledError:
+                pass
     except asyncio.TimeoutError:
         await edit_message_text(m, text='😟 Sorry! Video trimming failed due to timeout. Your process was taking too long to complete, hence cancelled')
         c.CURRENT_PROCESSES[chat_id] -= 1
