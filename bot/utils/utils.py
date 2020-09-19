8a67af9d5087e01bdff4b5505359ccde6ec8d542
odysseusmax <royschristy@gmail.com>
2020-09-19 12:27:24 +0530

    test new setup

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -46,7 +46,7 @@ async def run_subprocess(cmd):
 async def generate_thumbnail_file(file_path, uid):
     output_folder = Config.THUMB_OP_FLDR.joinpath(uid)
     os.makedirs(output_folder, exist_ok=True)
-    
+
     thumb_file = output_folder.joinpath('thumb.jpg')
     ffmpeg_cmd = f"ffmpeg -ss 0 -i '{file_path}' -vframes 1 -vf \"scale=320:-1\" -y '{thumb_file}'"
     output = await run_subprocess(ffmpeg_cmd)
@@ -60,7 +60,7 @@ def pack_id(msg):
     chat_id_offset = 2
     pack_bits = 32
     msg_id_offset = pack_bits + chat_id_offset
-    
+
     file_id |= msg.chat.id << chat_id_offset
     file_id |= msg.message_id << msg_id_offset
     return file_id
@@ -69,6 +69,26 @@ def pack_id(msg):
 def generate_stream_link(media_msg):
     file_id = pack_id(media_msg)
     return f"{Config.HOST}/stream/{file_id}"
+
+
+async def get_media_info(c, chat_id, message_id):
+    media_info_dir = Path(f"mediainfo")
+    os.makedirs(media_info_dir, exist_ok=True)
+    media_info_file = media_info_dir.joinpath(f'{}-{}.txt')
+    if media_info_file,exists():
+        return json.load(open(media_info_file, 'r'))
+
+    message = await c.get_message(chat_id, message_id)
+    if m.media:
+        file_link = generate_stream_link(m)
+    else:
+        file_link = m.text
+
+    ffprobe_cmd = f"ffprobe -v quiet -print_format json -show_format -show_streams {shlex.quote(file_link)}"
+    data, err = await run_subprocess(ffprobe_cmd)
+    media_info = json.loads(data.decode().rstrip())
+    json.dump(media_info, open(media_info_file, 'w'))
+    return media_info
 
 
 async def get_dimentions(input_file_link):
@@ -83,38 +103,35 @@ async def get_dimentions(input_file_link):
     return width, height
 
 
-async def get_duration(input_file_link):
-    ffmpeg_dur_cmd = f"ffprobe -v error -show_entries format=duration -of csv=p=0:s=x -select_streams v:0 {shlex.quote(input_file_link)}"
-    out, err = await run_subprocess(ffmpeg_dur_cmd)
-    log.debug(f"{out} \n {err}")
-    out = out.decode().strip()
-    if not out:
-        return err.decode()
-    duration = round(float(out))
-    if duration:
-        return duration
-    return 'No duration!'
+async def get_duration(media_info):
+    frmt = media_info.get('format')
+    if frmt is None:
+        return 'No duration!'
+    dur = frmt.get('duration')
+    if dur is None:
+        return 'No duration!'
+    return round(float(dur))
 
 
 async def fix_subtitle_codec(file_link):
     fixable_codecs = ['mov_text']
-    
+
     ffmpeg_dur_cmd = f"ffprobe -v error -select_streams s -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1  {shlex.quote(file_link)}"
-    
+
     out, err = await run_subprocess(ffmpeg_dur_cmd)
     log.debug(f"{out} \n {err}")
     out = out.decode().strip()
     if not out:
         return ''
-    
+
     fix_cmd = ''
     codecs = [i.strip() for i in out.split('\n')]
     for indx, codec in enumerate(codecs):
         if any(fixable_codec in codec for fixable_codec in fixable_codecs):
             fix_cmd += f'-c:s:{indx} srt '
-    
+
     return fix_cmd
-    
+
 
 async def edit_message_text(m, **kwargs):
     while True:
@@ -128,7 +145,7 @@ async def edit_message_text(m, **kwargs):
 
 async def display_settings(c, m, cb=False):
     chat_id = m.from_user.id if cb else m.chat.id
-    
+
     as_file = await c.db.is_as_file(chat_id)
     as_round = await c.db.is_as_round(chat_id)
     watermark_text = await c.db.get_watermark_text(chat_id)
@@ -136,7 +153,7 @@ async def display_settings(c, m, cb=False):
     watermark_color_code = await c.db.get_watermark_color(chat_id)
     screenshot_mode = await c.db.get_screenshot_mode(chat_id)
     font_size = await c.db.get_font_size(chat_id)
-    
+
     sv_btn = [
         InlineKeyboardButton("Sample Video Duration", 'rj'),
         InlineKeyboardButton(f"{sample_duration}s", 'set+sv')
@@ -152,25 +169,25 @@ async def display_settings(c, m, cb=False):
     as_file_btn = [InlineKeyboardButton("Upload Mode", 'rj')]
     wm_btn = [InlineKeyboardButton("Watermark", 'rj')]
     sm_btn = [InlineKeyboardButton("Screenshot Generation Mode", 'rj')]
-    
-    
+
+
     if as_file:
         as_file_btn.append(InlineKeyboardButton("📁 Uploading as Document.", 'set+af'))
     else:
         as_file_btn.append(InlineKeyboardButton("🖼️ Uploading as Image.", 'set+af'))
-    
+
     if watermark_text:
         wm_btn.append(InlineKeyboardButton(f"{watermark_text}", 'set+wm'))
     else:
         wm_btn.append(InlineKeyboardButton("No watermark exists!", 'set+wm'))
-    
+
     if screenshot_mode == 0:
         sm_btn.append(InlineKeyboardButton("Equally spaced screenshots", 'set+sm'))
     else:
         sm_btn.append(InlineKeyboardButton("Random screenshots", 'set+sm'))
-    
+
     settings_btn = [as_file_btn, wm_btn, wc_btn, fs_btn, sv_btn, sm_btn]
-    
+
     if cb:
         try:
             await m.edit_message_reply_markup(
@@ -179,7 +196,7 @@ async def display_settings(c, m, cb=False):
         except:
             pass
         return
-    
+
     await m.reply_text(
         text = f"Here You can configure my behavior.",
         quote=True,
@@ -204,4 +221,5 @@ def gen_ik_buttons():
             btns.append(i_keyboard)
     btns.append([InlineKeyboardButton('Manual Screenshots!', 'mscht')])
     btns.append([InlineKeyboardButton('Trim Video!', 'trim')])
+    btns.append([InlineKeyboardButton('Get Media Information', 'mi')])
     return btns
