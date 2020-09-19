1d2749521e7f1cb4c6d3c1738a02900b60561629
odysseusmax <royschristy@gmail.com>
2020-09-19 13:30:14 +0530

    add web view for mediainfo

--- bot/plugins/mediainfo.py
+++ bot/plugins/mediainfo.py
@@ -1,7 +1,10 @@
 import io
+import os
 import json
 
 from pyrogram import filters as  Filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import aiohttp
 
 from ..screenshotbot import ScreenShotBot
 from ..config import Config
@@ -23,6 +26,25 @@ async def _(c, m):
     media_info = await get_media_info(file_link)
     media_info_file = io.BytesIO()
     media_info_file.name = "mediainfo.json"
-    media_info_file.write(json.dumps(media_info).encode())
+    media_info_file.write(json.dumps(media_info, indent=4).encode())
     await m.edit_message_text(text='Your media info will be send here shortly!')
-    await c.send_document(chat_id=m.from_user.id, document=media_info_file)
+    await c.send_document(chat_id=m.from_user.id, document=media_info_file,
+                          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Get Web URL', 'webmi')]]))
+
+
+@ScreenShotBot.on_callback_query(Filters.create(lambda _, __, query: query.data.startswith('webmi')))
+async def __(c, m):
+    # https://github.com/eyaadh/megadlbot_oss/blob/306fb21dbdbdc8dc17294a6cb7b7cdafb11e44da/mega/helpers/media_info.py#L30
+
+    media_info = await m.message.download()
+    neko_endpoint = "https://nekobin.com/api/documents"
+    async with aiohttp.ClientSession() as nekoSession:
+        payload = {"content": open(media_info, 'r').read()}
+        async with nekoSession.post(neko_endpoint, data=payload) as resp:
+            resp = await resp.json()
+            neko_link = f"https://nekobin.com/{resp['result']['key']}"
+    await m.edit_message_reply_markup(InlineKeyboardMarkup([[InlineKeyboardButton('Web URL', neko_link)]]))
+    try:
+        os.remove(media_info)
+    except:
+        pass
