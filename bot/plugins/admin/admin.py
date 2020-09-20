6eefca49bcab288aa0aeeeb8cb864a1663d651a8
odysseusmax <royschristy@gmail.com>
2020-09-20 09:15:03 +0530

    add /admin command

---
+++ bot/plugins/admin/admin.py
@@ -0,0 +1,17 @@
+from pyrogram import filters as Filters
+
+from bot.config import Config
+from bot.screenshotbot import ScreenShotBot
+
+
+@ScreenShotBot.on_message(Filters.private & Filters.command("admin") & Filters.user(Config.AUTH_USERS))
+async def admin(c, m):
+
+    text = 'Current admins of the bot:\n\n'
+    async for admn in c.get_users(Config.AUTH_USERS):
+        text += f'\t- {admn.mention}\n'
+
+    text += "\nAvailable admin commands are:\n"
+    text += "\t- /unban_user\n\t- /broadcast\n\t- /banned_users\n\t- /ban_user\n\t- /status"
+
+    await m.reply_text(text, quote=True)
