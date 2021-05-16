ebe5754c047a20ec607fa8985146e1f8ab58ca1d
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-16 10:15:59 +0530

    Update status.py

--- bot/plugins/admin/status.py
+++ bot/plugins/admin/status.py
@@ -1,17 +1,11 @@
-from pyrogram import filters
+from pyrogram import filters, Client
 
 from bot.config import Config
-from bot.database import Database
-from bot.screenshotbot import ScreenShotBot
-
-db = Database()
 
 
-@ScreenShotBot.on_message(
-    filters.private & filters.command("status") & filters.user(Config.AUTH_USERS)
-)
+@Client.on_message(filters.private & filters.command("status") & filters.user(Config.AUTH_USERS))
 async def sts(c, m):
-    total_users = await db.total_users_count()
+    total_users = await c.db.total_users_count()
     text = f"Total user(s) till date: {total_users}\n\n"
-    text += f"Active users, today: {len(c.CHAT_FLOOD)}"
+    text += f"Bot Updates Active Users: {len(c.CHAT_FLOOD)}"
     await m.reply_text(text=text, quote=True)
