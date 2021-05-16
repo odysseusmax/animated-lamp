66ad5a508cd4b3794b98bc9a4274ef852ad4323b
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-16 10:17:31 +0530

    Update status.py

--- bot/plugins/admin/status.py
+++ bot/plugins/admin/status.py
@@ -1,11 +1,11 @@
 from pyrogram import filters, Client
-
-from bot.config import Config
+from renamer.config import Config
 
 
 @Client.on_message(filters.private & filters.command("status") & filters.user(Config.AUTH_USERS))
 async def sts(c, m):
     total_users = await c.db.total_users_count()
+    bot_updates = await c.db.
     text = f"Total user(s) till date: {total_users}\n\n"
     text += f"Bot Updates Active Users: {len(c.CHAT_FLOOD)}"
     await m.reply_text(text=text, quote=True)
